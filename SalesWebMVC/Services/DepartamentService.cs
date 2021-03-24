b5048d5b2f0d559dc3122fa0d3cc814f9de14927
diogosalvadorb <60317215+diogosalvadorb@users.noreply.github.com>
2021-03-24 17:52:59 -0300

    SellerForm

---
+++ SalesWebMVC/Services/DepartamentService.cs
@@ -0,0 +1,23 @@
+﻿using SalesWebMVC.Data;
+using SalesWebMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMVC.Services
+{
+    public class DepartamentService
+    {
+        private readonly SalesWebMVCContext _context;
+
+        public DepartamentService(SalesWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        public List<Departament> FindAll()
+        {
+            return _context.Departament.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
